256b53a222c48ea2c42cf6d9029e0da93589045c
folk113 <folk113@folk113deMac-Pro.local>
2014-09-03 23:38:08 +0800

    ./

--- UctApp/GroupViewController.swift
+++ UctApp/GroupViewController.swift
@@ -11,25 +11,27 @@ import UIKit
 class GroupViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     @IBOutlet var tableView:UITableView?
-    let identifier = "cell";
-    var indexSet:NSIndexSet;
-    var selectedIndex:Int = -1;
+    let identifier = "cell"
+    var indexSet:NSIndexSet
+    var selectedIndex:Int = -1
+    
+    var gdata:NSMutableArray = NSMutableArray()
+    var udata:NSArray = NSArray(array: ["成员1","成员2","成员3"])
     
     required init(coder aDecoder: NSCoder) {
         indexSet = NSIndexSet(index:1)
-        super.init(coder: aDecoder)//        fatalError("init(coder:) has not been implemented")
-        
+        super.init(coder: aDecoder)
+        //fatalError("init(coder:) has not been implemented")
+        gdata.addObjectsFromArray(["一组","二组","三组"])
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         var nib:UINib = UINib(nibName: "GroupCellView", bundle: nil)
-        self.tableView?.registerNib(nib, forCellReuseIdentifier: identifier)
+        self.tableView!.registerNib(nib, forCellReuseIdentifier: identifier)
         self.tableView!.delegate = self
         self.tableView!.dataSource = self
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,36 +52,47 @@ class GroupViewController: UIViewController,UITableViewDelegate,UITableViewDataS
     
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
     {
-//        if selectedIndex > 0
-//        {
-//            return 6
-//        }
-        return 5
+//        return gdata.count
+        return numInsert
     }
     
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
     // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-    
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
     {
         var cell:UITableViewCell = self.tableView?.dequeueReusableCellWithIdentifier(identifier) as UITableViewCell
-//        cell.backgroundColor = UIColor.blueColor()
+        
+        cell.textLabel.text = "label"//gdata.objectAtIndex(indexPath.row) as NSString
+        cell.imageView.image = UIImage(named: "second")        
+//        cell.detailTextLabel!.text = "detail"
         return cell
     }
     
+    let delayInSeconds = 0
     
     
+    var numInsert:Int = 8;
     func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!)
     {
-        tableView.insertSections(indexSet, withRowAnimation: UITableViewRowAnimation.None)
-        selectedIndex = 1
+        self.tableView!.beginUpdates()
+
+//        var sections: NSIndexSet! = NSIndexSet(indexesInRange: NSMakeRange(0,3))
+//        numInsert+=3
+//        self.tableView!.insertSections(sections, withRowAnimation: UITableViewRowAnimation.Top)
+//        self.tableView!.insertRowsAtIndexPaths(indexPaths: [AnyObject]!, withRowAnimation: <#UITableViewRowAnimation#>)
+        
+       
+
+        numInsert -= 1
+        
+        self.tableView!.deleteRowsAtIndexPaths(NSArray().arrayByAddingObject(indexPath), withRowAnimation:UITableViewRowAnimation.Bottom)
+        self.tableView!.endUpdates()
     }
     
     func tableView(tableView: UITableView!, didDeselectRowAtIndexPath indexPath: NSIndexPath!)
     {
-        tableView.deleteSections(indexSet, withRowAnimation: UITableViewRowAnimation.None)
-        selectedIndex = -1
+//        tableView.deleteRowsAtIndexPaths(udata, withRowAnimation: UITableViewRowAnimation.None)
+        gdata.removeObjectsInArray(udata)
     }
-
 
 }
