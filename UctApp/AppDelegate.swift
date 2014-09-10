a2f5e00027dff33b99aac3d6de3542377a814c98
ptyt <ptyt@ptytdeMac-mini.local>
2014-09-10 17:49:43 +0800

    添加一些权限授权

--- UctApp/AppDelegate.swift
+++ UctApp/AppDelegate.swift
@@ -12,6 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
+    
+    var bgTask:UIBackgroundTaskIdentifier = 0
+    
+    var counter:Int = 0
 
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
@@ -23,14 +27,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
+    
+    func backgroundHandler()
+    {
+        NSLog("### -->backgroundinghandler");
 
+        var app:UIApplication = UIApplication.sharedApplication()
+
+        bgTask = app.beginBackgroundTaskWithExpirationHandler({
+
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+                
+                while (true)
+                {
+                    NSLog("counter:%d", self.counter++)
+                    sleep(1)
+                }
+                
+                });
+
+        })
+
+        app.endBackgroundTask(bgTask)
+        bgTask = UIBackgroundTaskInvalid
+    }
+    
     func applicationDidEnterBackground(application: UIApplication!) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        UIApplication.sharedApplication().setKeepAliveTimeout(600, handler:{ self.backgroundHandler }())
+        NSLog("enter background")
     }
 
     func applicationWillEnterForeground(application: UIApplication!) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        NSLog("enter foreground")
+        UIApplication.sharedApplication().clearKeepAliveTimeout()
     }
 
     func applicationDidBecomeActive(application: UIApplication!) {
